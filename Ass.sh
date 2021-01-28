85a699192123f56725004aab1191830cf9f8e491
siddharth.dh <siddharth.dh@media.net>
2021-01-28 21:47:22 +0530

    lyl

--- Ass.sh
+++ Ass.sh
@@ -5,7 +5,8 @@ for filename in $(ls $source); do
 	size=$(du --apparent-size $source$filename | awk '{ print $1 }')
 	if [ $size -ge $threshold ]
 	then
-		tar -cf $archive$filename.tgz $source$filename 
+		datetime=$(date +%d%m%Y%H%M%S | sed 's/\(:[0-9][0-9]\)[0-9]*$/\1/')
+		tar -cf $archive$filename\_$datetime.tgz $source$filename 
 		echo "greater, so compressing : " $filename
 	else
 		echo "smaller, so ignoring : " $filename
