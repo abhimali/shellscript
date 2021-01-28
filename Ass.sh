99a6ca8fba93b16f2d46aab0c33b6438a702f2e6
Abhijeet <abhijeetmali268@gmail.com>
2021-01-28 22:11:16 +0530

    Maybe second last

--- Ass.sh
+++ Ass.sh
@@ -3,9 +3,10 @@ source="./Source/"
 archive="./Archive/"
 for filename in $(ls $source); do
 	size=$(du --apparent-size $source$filename | awk '{ print $1 }')
+	#echo $source$filename
 	if [ $size -ge $threshold ]
 	then
-		datetime=$(date +%d%m%Y%H%M%S | sed 's/\(:[0-9][0-9]\)[0-9]*$/\1/')
+		datetime=$(date +%d%m%Y%H%M%S)
 		tar -cf $archive$filename\_$datetime.tgz $source$filename 
 		echo "greater, so compressing : " $filename
 	else
