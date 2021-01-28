0257834ac5018490cbff5073eea3a4e5eac8b0ce
siddharth.dh <siddharth.dh@media.net>
2021-01-28 21:36:30 +0530

    lel

--- Ass.sh
+++ Ass.sh
@@ -5,9 +5,9 @@ for filename in $(ls $source); do
 	size=$(du --apparent-size $source$filename | awk '{ print $1 }')
 	if [ $size -ge $threshold ]
 	then
-		tar -cvf $archive$filename.tgz $source$filename 
+		tar -cf $archive$filename.tgz $source$filename 
 		echo "greater, so compressing : " $filename
 	else
 		echo "smaller, so ignoring : " $filename
 	fi
-done;
+done;	
