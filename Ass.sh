00b153a1b1fb9d8e091e43f6839a7aa2a1e48d6b
siddharth.dh <siddharth.dh@media.net>
2021-01-28 21:31:01 +0530

    lul

--- Ass.sh
+++ Ass.sh
@@ -1,9 +1,14 @@
 threshold=5
-for filename in ./Source/*.txt; do
-	size=$(du --apparent-size $filename | awk '{ print $1 }')
+source="./Source/"
+archive="./Archive/"
+for filename in $(ls $source); do
+	echo filename $filename
+	size=$(du --apparent-size $source$filename | awk '{ print $1 }')
+	echo size $size 
 	if [ $size -ge $threshold ]
 	then
-		tar cfz ./Archive/$filename.tgz $filename 
+		tar -cvf $archive$filename.tgz $source$filename 
+		echo "greater" $filename
 	else
 		echo "smaller" $filename
 	fi
