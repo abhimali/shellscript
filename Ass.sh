90e9f3ad7717ae17ec8bdd17c1e5f64c4ec84e1c
siddharth.dh <siddharth.dh@media.net>
2021-01-28 21:02:31 +0530

    blah

--- Ass.sh
+++ Ass.sh
@@ -1,3 +1,10 @@
-find * -type f -size +10000c 
--exec mv ~/home/archive  {}  \:
-
+threshold=5
+for filename in ./Source/*.txt; do
+	size=$(du --apparent-size $filename | awk '{ print $1 }')
+	if [ $size -ge $threshold ]
+	then
+		tar cfz ./Archive/$filename.tgz $filename 
+	else
+		echo "smaller" $filename
+	fi
+done;
