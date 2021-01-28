501a27eb0e8e5bf6260bf2aadfc583b15baa1345
siddharth.dh <siddharth.dh@media.net>
2021-01-28 23:11:41 +0530

    done

--- Ass.sh
+++ Ass.sh
@@ -1,15 +1,22 @@
 threshold=5
 source="./Source/"
 archive="./Archive/"
+name="compressed.tgz"
+# var=$(cat config.csv | awk -F '=' '{print $2}')
+
 for filename in $(ls $source); do
 	size=$(du --apparent-size $source$filename | awk '{ print $1 }')
 	#echo $source$filename
 	if [ $size -ge $threshold ]
 	then
 		datetime=$(date +%d%m%Y%H%M%S)
-		tar -cf $archive$filename\_$datetime.tgz $source$filename 
+		mv $source$filename $archive$filename\_$datetime
 		echo "greater, so compressing : " $filename
 	else
 		echo "smaller, so ignoring : " $filename
 	fi
-done;	
+done;
+
+tar -zcf $archive$name $archive
+rm $archive/file*
+# rm $archive/*.txt
